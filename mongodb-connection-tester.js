0038383ff8481278efc0fe9847c3f13c52e7dc85
Sujit Patel <sujit.patel@nurture.farm>
2022-09-07 00:12:49 +0530

    adding mongodb-connection-tester.js

---
+++ mongodb-connection-tester.js
@@ -0,0 +1,43 @@
+/**
+ * This Script test mongodb connection and list all the database names.
+ * Steps:
+ * 1. npm install mongodb
+ * 2. node mongodb-connection-tester.js
+ */
+
+const {MongoClient} = require('mongodb');
+
+async function listDatabases(client){
+    databasesList = await client.db().admin().listDatabases();
+ 
+    console.log("Databases:");
+    databasesList.databases.forEach(db => console.log(` - ${db.name}`));
+};
+
+async function main(){
+    /**
+     * Connection URI. Update <username>, <password>, <ip>, <port> and <dbName> to reflect your cluster.
+     * Update authSource in case your user in diffrent database generally not needed.
+     * See https://docs.mongodb.com/ecosystem/drivers/node/ for more details
+     */
+
+    const uri = "mongodb://<userName>:<password>@<ip>:<port>/<dbName>?authSource=admin&retryWrites=true&w=majority";
+ 
+
+    const client = new MongoClient(uri);
+ 
+    try {
+        // Connect to the MongoDB cluster
+        await client.connect();
+ 
+        // Make the appropriate DB calls
+        await  listDatabases(client);
+ 
+    } catch (e) {
+        console.error(e);
+    } finally {
+        await client.close();
+    }
+}
+
+main().catch(console.error);
